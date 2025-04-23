b66dd9696cb916f6273e2b111e3c9f4de9fdd745
Your Name <your-email@example.com>
2025-04-24 00:27:04 +0530

    commented link code

--- webapp/app/dashboard/Dashboard.js
+++ webapp/app/dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import Link from 'next/link';
+// import Link from 'next/link';
 import { motion } from 'framer-motion';
 // import { useRouter } from "next/navigation";
 import CreateSectionModal from '@/components/CreateSectionModal';
